d6c44d78efd6b5fc9a9c6e4be54d4467ce342bb3
Franco <francozuccorononno@gmail.com>
2024-06-28 14:26:37 +0200

    Ejercicio 7 y 7b

---
+++ MODULO_1/UNIDAD_2_JS/007_Bucle_For_Sencillo/main.js
@@ -0,0 +1,10 @@
+for (let i = 0; i <= 4; i++) {
+    document.write("<h1>" + i + "</h1><br>");
+}
+
+for (let y = 0; y <= 4; y++) {
+    if (y === 2) {
+        break;
+    }
+    document.write("<h2>" + y + "</h2><br>");
+}
