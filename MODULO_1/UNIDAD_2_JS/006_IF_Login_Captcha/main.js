c337d4aeebcf421b9fc48452106e768ffa6463c3
Franco <francozuccorononno@gmail.com>
2024-07-01 10:09:32 +0200

    Fix Ejercicio 006

--- MODULO_1/UNIDAD_2_JS/006_IF_Login_Captcha/main.js
+++ MODULO_1/UNIDAD_2_JS/006_IF_Login_Captcha/main.js
@@ -6,6 +6,7 @@ if (resUsuario === numCorrecto) {
     document.write('<h1>Bienvenido!');
 } else if (isNaN(resUsuario)) {
     alert('Debes introducir un numero');
+    window.location.reload();
 } else {
     alert('Incorrecto');
     window.location.reload();
