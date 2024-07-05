41dc71a050221ff1a3f9d602bbfd605bccc636dd
Franco <francozuccorononno@gmail.com>
2024-07-05 11:11:04 +0200

    Ejercicio 16 JS

---
+++ MODULO_1/UNIDAD_2_JS/016_Objeto_Persona/main.js
@@ -0,0 +1,11 @@
+const persona = {
+    nombre: "Juan",
+    apellido: "Nadie",
+    edad: 50,
+    ojosColor: {
+        izquierdo: "Verde",
+        derecho: "Azul"
+    },
+    altura: 1.80
+};
+
