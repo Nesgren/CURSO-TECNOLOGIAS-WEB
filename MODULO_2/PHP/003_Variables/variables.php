f28c56d3239f2bcff4d2198315436065f9ac3353
Nesgren <francozuccorononno@gmail.com>
2024-07-23 12:37:27 +0200

    Variables

---
+++ MODULO_2/PHP/003_Variables/variables.php
@@ -0,0 +1,10 @@
+<?php
+$nombre = "Franco";
+$edad = 30;
+$variable1 = "Mis \"super\" amigos";
+$variable2 = 'Mis "super" amigos';
+echo 'Mi nombre es: '.$nombre."<BR>";
+echo $edad."<BR>";
+echo $variable1."<BR>";
+echo $variable2."<BR>";
+?>
