885712cb531b57950752f09e2286122736f5f546
Nesgren <francozuccorononno@gmail.com>
2024-07-23 10:13:49 +0200

    Div Color

--- MODULO_2/PHP/002_Formulario/procesar.php
+++ MODULO_2/PHP/002_Formulario/procesar.php
@@ -1,3 +1,4 @@
 Hola <?php echo $_POST['nombre']; ?>.
 Usted tiene <?php echo $_POST['edad']; ?> años
 Su color favorito es <?php echo $_POST['color']; ?>.
+<div class="colored-div" style="background-color: <?php echo $color; ?>;"></div>
