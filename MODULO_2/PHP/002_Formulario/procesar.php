b4afdabe6d9da8c16e6c780919de0c3df75ef41b
Nesgren <francozuccorononno@gmail.com>
2024-07-23 10:22:20 +0200

    etiquetas html

--- MODULO_2/PHP/002_Formulario/procesar.php
+++ MODULO_2/PHP/002_Formulario/procesar.php
@@ -1,3 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    Hola <?php echo $nombre; ?>.
+    Usted tiene <?php echo $edad; ?> años.
+    Su color favorito es <?php echo $color; ?>.
+    <div class="colored-div" style="background-color: <?php echo $color; ?>;"></div>
+</body>
+</html>
 <style>
     .colored-div {
         width: 100px;
@@ -5,7 +19,3 @@
     }
 </style>
 
-Hola <?php echo $nombre; ?>.
-Usted tiene <?php echo $edad; ?> años.
-Su color favorito es <?php echo $color; ?>.
-<div class="colored-div" style="background-color: <?php echo $color; ?>;"></div>
