6e490aeeb39e7a428732d12402505d4e239a265f
Nesgren <francozuccorononno@gmail.com>
2024-07-23 13:31:15 +0200

    Ejercicio 1

---
+++ MODULO_2/PHP/004_Ejercicio/procesar.php
@@ -0,0 +1,12 @@
+<?php
+$nombre = htmlspecialchars($_POST['nombre']);
+$email = htmlspecialchars($_POST['email']);
+$opciones = htmlspecialchars($_POST['opciones']);
+$comentarios = htmlspecialchars($_POST['comentarios']);
+
+echo "<h1>Datos Enviados</h1>";
+echo "<p><strong>Nombre:</strong> " . $nombre . "</p>";
+echo "<p><strong>Correo Electrónico:</strong> " . $email . "</p>";
+echo "<p><strong>Opción Seleccionada:</strong> " . $opciones . "</p>";
+echo "<p><strong>Comentarios:</strong> " . nl2br($comentarios) . "</p>";
+?>
