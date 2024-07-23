1d09457905ad39929c0308fc5273be393434a4ea
Nesgren <francozuccorononno@gmail.com>
2024-07-23 10:38:59 +0200

    ahora si

--- MODULO_2/PHP/002_Formulario/procesar.php
+++ MODULO_2/PHP/002_Formulario/procesar.php
@@ -1,3 +1,5 @@
-Hola <?php echo $_POST['nombre']; ?>.
-Usted tiene <?php echo $_POST['edad']; ?> años
-Su color favorito es <div class="colored-div" style="background-color: <?php echo $color; ?>;"></div>.
+<?php
+echo "Hola " . $_POST['nombre'] . ".<br>";
+echo "Usted tiene " . $_POST['edad'] . " años.<br>";
+echo "Su color favorito es <div style='width: 50px; height: 50px; background-color: " . $_POST['color'] . ";'></div>.";
+?>
