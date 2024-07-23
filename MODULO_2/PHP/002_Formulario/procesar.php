b80a65fd978c2ceb506c38361a04727a90bdb0b1
Nesgren <francozuccorononno@gmail.com>
2024-07-23 10:24:44 +0200

    esta si

--- MODULO_2/PHP/002_Formulario/procesar.php
+++ MODULO_2/PHP/002_Formulario/procesar.php
@@ -1,21 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    Hola <?php echo $nombre; ?>.
-    Usted tiene <?php echo $edad; ?> años.
-    Su color favorito es <?php echo $color; ?>.
-    <div class="colored-div" style="background-color: <?php echo $color; ?>;"></div>
-</body>
-</html>
-<style>
-    .colored-div {
-        width: 100px;
-        height: 100px;
-    }
-</style>
-
+Hola <?php echo $_POST['nombre']; ?>.
+Usted tiene <?php echo $_POST['edad']; ?> años
+Su color favorito es <div class="colored-div" style="background-color: <?php echo $color; ?>;"></div>.
