7469f965f369b5561e2d4d94e737c290ec94ebb3
Nesgren <francozuccorononno@gmail.com>
2024-07-23 11:36:54 +0200

    Metodo GET

--- MODULO_2/PHP/002_Formulario/procesar.php
+++ MODULO_2/PHP/002_Formulario/procesar.php
@@ -1,5 +1,5 @@
 <?php
-echo "Hola " . $_POST['nombre'] . ".<br>";
-echo "Usted tiene " . $_POST['edad'] . " años.<br>";
-echo "Su color favorito es <div style='width: 50px; height: 50px; background-color: " . $_POST['color'] . ";'></div>.";
+echo "Hola " . $_GET['nombre'] . ".<br>";
+echo "Usted tiene " . $_GET['edad'] . " años.<br>";
+echo "Su color favorito es <div style='width: 50px; height: 50px; background-color: " . $_GET['color'] . ";'></div>.";
 ?>
