4eb62fff308f711a2f5a302756032922af5b69ff
Nesgren <francozuccorononno@gmail.com>
2024-07-23 12:17:34 +0200

    htmlspecialchars added

--- MODULO_2/PHP/002_Formulario/procesar.php
+++ MODULO_2/PHP/002_Formulario/procesar.php
@@ -1,5 +1,5 @@
 <?php
-echo "Hola " . $_GET['nombre'] . ".<br>";
-echo "Usted tiene " . $_GET['edad'] . " años.<br>";
-echo "Su color favorito es <div style='width: 50px; height: 50px; background-color: " . $_GET['color'] . ";'></div>.";
+echo "Hola " . htmlspecialchars ($_GET['nombre']) . ".<br>";
+echo "Usted tiene " . htmlspecialchars($_GET['edad']) . " años.<br>";
+echo "Su color favorito es <div style='width: 50px; height: 50px; background-color: " . htmlspecialchars($_GET['color']) . ";'></div>.";
 ?>
