8281252d3cde65e8b328a3a9f47f42b88610a23c
Nesgren <francozuccorononno@gmail.com>
2024-07-24 11:08:08 +0200

    Styles added

--- MODULO_2/PHP/005_Ejercicio_2/suma.php
+++ MODULO_2/PHP/005_Ejercicio_2/suma.php
@@ -7,3 +7,17 @@
     echo $area;
 ?>
 </pre>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Area</title>
+    <liink rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <h1>Area</h1>
+    <p>El area del rectangulo es: <?php echo $area ?></p>
+</body>
+</html>
