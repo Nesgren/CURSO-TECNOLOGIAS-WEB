bb37c4fdd5b27346094c68ad9b08312fd3cee09b
Nesgren <francozuccorononno@gmail.com>
2024-07-24 11:10:43 +0200

    Fix

--- MODULO_2/PHP/005_Ejercicio_2/suma.php
+++ MODULO_2/PHP/005_Ejercicio_2/suma.php
@@ -14,7 +14,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Area</title>
-    <liink rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
     <h1>Area</h1>
