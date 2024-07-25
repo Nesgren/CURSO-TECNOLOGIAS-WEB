3e2d85617504ae5793190e2e2c1648c83efe43a3
Nesgren <francozuccorononno@gmail.com>
2024-07-25 10:59:50 +0200

    Ejercicio 6

---
+++ MODULO_2/PHP/009_Ejercicio_6/calcular.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Resultado de la Compra</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <h1>Resultado de la Compra</h1>
+    <?php
+        $cantidad = $_POST['cantidad'];
+        $precio = $_POST['precio'];
+        $descuento = 0;
+
+        switch ($cantidad) {
+            case 2:
+                $descuento = 0.10;
+                break;
+            case 3:
+                $descuento = 0.15;
+                break;
+            case 4:
+                $descuento = 0.20;
+                break;
+            case 5:
+            default:
+                $descuento = 0.25;
+                break;
+        }
+        $total = $cantidad * $precio * (1 - $descuento);
+        echo "<p>Total a pagar por {$cantidad} entradas: $" . ($total) . "</p>";
+    ?>
+    <a href="index.html">Volver a la página de compra</a>
+</body>
+</html>
