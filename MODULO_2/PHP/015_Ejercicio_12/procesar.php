69dd33511ad9c9743926cf229b24b86abee20514
Nesgren <francozuccorononno@gmail.com>
2024-07-29 09:40:20 +0200

    data fix

--- MODULO_2/PHP/015_Ejercicio_12/procesar.php
+++ MODULO_2/PHP/015_Ejercicio_12/procesar.php
@@ -10,12 +10,12 @@
     <h1>Datos Ingresados en Orden Inverso</h1>
     <ul>
         <?php
-        if (isset($_POST['data'])) {
-            $data = $_POST['data'];
-            $dataLength = count($data);
+        if (isset($_POST['dato'])) {
+            $dato = $_POST['dato'];
+            $datoLength = count($dato);
             
-            for ($i = $dataLength - 1; $i >= 0; $i--) {
-                echo "<li>" . htmlspecialchars($data[$i]) . "</li>";
+            for ($i = $datoLength - 1; $i >= 0; $i--) {
+                echo "<li>" . htmlspecialchars($dato[$i]) . "</li>";
             }
         } else {
             echo "<p>No se recibieron datos.</p>";
