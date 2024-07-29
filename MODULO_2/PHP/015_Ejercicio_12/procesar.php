8ea1f47e54b6554fd5dd69e5c118be902d5690b1
Nesgren <francozuccorononno@gmail.com>
2024-07-29 09:46:51 +0200

    Ejercicio 12 fix

--- MODULO_2/PHP/015_Ejercicio_12/procesar.php
+++ MODULO_2/PHP/015_Ejercicio_12/procesar.php
@@ -10,15 +10,25 @@
     <h1>Datos Ingresados en Orden Inverso</h1>
     <ul>
         <?php
-        if (isset($_POST['dato'])) {
-            $dato = $_POST['dato'];
-            $datoLength = count($dato);
+        if (isset($_POST['dato1']) && isset($_POST['dato2']) && isset($_POST['dato3']) && 
+            isset($_POST['dato4']) && isset($_POST['dato5']) && isset($_POST['dato6'])) {
             
-            for ($i = $datoLength - 1; $i >= 0; $i--) {
-                echo "<li>" . htmlspecialchars($dato[$i]) . "</li>";
+            $datos = [
+                $_POST['dato1'],
+                $_POST['dato2'],
+                $_POST['dato3'],
+                $_POST['dato4'],
+                $_POST['dato5'],
+                $_POST['dato6']
+            ];
+            
+            $datosLength = count($datos);
+            
+            for ($i = $datosLength - 1; $i >= 0; $i--) {
+                echo "<li>" . htmlspecialchars($datos[$i]) . "</li>";
             }
         } else {
-            echo "<p>No se recibieron datos.</p>";
+            echo "<p>No se recibieron todos los datos.</p>";
         }
         ?>
     </ul>
