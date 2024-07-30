900c138b57a0d71546907540c93232e716be4807
Nesgren <francozuccorononno@gmail.com>
2024-07-30 11:17:07 +0200

    styles fix

--- MODULO_2/PHP/017_Ejercicio_14/index.php
+++ MODULO_2/PHP/017_Ejercicio_14/index.php
@@ -14,11 +14,41 @@
         <div class="car-list">
             <?php
             $coches = [
-                ["marca" => "Volkswagen", "modelo" => "Golf", "año" => 2021, "color" => "Gris", "matricula" => "DE-456-FG"],
-                ["marca" => "BMW", "modelo" => "Serie 3", "año" => 2019, "color" => "Blanco", "matricula" => "HI-789-JK"],
-                ["marca" => "Renault", "modelo" => "Clio", "año" => 2020, "color" => "Rojo", "matricula" => "LM-123-NO"],
-                ["marca" => "Peugeot", "modelo" => "208", "año" => 2018, "color" => "Azul", "matricula" => "PQ-456-RS"],
-                ["marca" => "Audi", "modelo" => "A4", "año" => 2022, "color" => "Negro", "matricula" => "TU-789-VW"]
+                [
+                    "marca" => "Volkswagen",
+                    "modelo" => "Golf",
+                    "año" => 2021,
+                    "color" => "Gris",
+                    "matricula" => "DE-456-FG"
+                ],
+                [
+                    "marca" => "BMW",
+                    "modelo" => "Serie 3",
+                    "año" => 2019,
+                    "color" => "Blanco",
+                    "matricula" => "HI-789-JK"
+                ],
+                [
+                    "marca" => "Renault",
+                    "modelo" => "Clio",
+                    "año" => 2020,
+                    "color" => "Rojo",
+                    "matricula" => "LM-123-NO"
+                ],
+                [
+                    "marca" => "Peugeot",
+                    "modelo" => "208",
+                    "año" => 2018,
+                    "color" => "Azul",
+                    "matricula" => "PQ-456-RS"
+                ],
+                [
+                    "marca" => "Audi",
+                    "modelo" => "A4",
+                    "año" => 2022,
+                    "color" => "Negro",
+                    "matricula" => "TU-789-VW"
+                ]
             ];
 
             foreach ($coches as $coche) {
