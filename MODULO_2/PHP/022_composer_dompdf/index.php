575b6ad200938bb934198a4e8f79a3f03e7a7c2d
Nesgren <francozuccorononno@gmail.com>
2024-08-02 10:39:32 +0200

    dompdf

---
+++ MODULO_2/PHP/022_composer_dompdf/index.php
@@ -0,0 +1,18 @@
+<?php
+// reference the Dompdf namespace
+use Dompdf\Dompdf;
+
+// instantiate and use the dompdf class
+$dompdf = new Dompdf();
+$dompdf->getOptions()->setChroot('/path/to/common/assets-directory');
+$dompdf->loadHtml('<strong>hello world</strong>');
+
+// (Optional) Setup the paper size and orientation
+$dompdf->setPaper('A4', 'landscape');
+
+// Render the HTML as PDF
+$dompdf->render();
+
+// Output the generated PDF to Browser
+$dompdf->stream();
+?>
