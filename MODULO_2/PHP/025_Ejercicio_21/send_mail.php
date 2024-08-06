7627a482b9f0e44bfa9ae180769a698f2bbf0179
Nesgren <francozuccorononno@gmail.com>
2024-08-06 21:22:53 +0200

    Ejercicio 21

---
+++ MODULO_2/PHP/025_Ejercicio_21/send_mail.php
@@ -0,0 +1,50 @@
+<?php
+require '../../../../vendor/autoload.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\SMTP;
+use Dompdf\Dompdf;
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+
+    $dompdf = new Dompdf();
+    $dompdf->loadHtml($message);
+    $dompdf->setPaper('A4', 'landscape');
+    $dompdf->render();
+    $output = $dompdf->output();
+    $nombreArchivo = 'expediente-' . time() . '.pdf';
+    file_put_contents('./pdfs/' . $nombreArchivo, $output);
+
+    $mail = new PHPMailer(true);
+
+    try {
+        $mail->isSMTP();
+        $mail->Host = 'smtp-relay.gmail.com';
+        $mail->SMTPAuth = true;
+        $mail->Username = 'sirena5584liriope@gmail.com'; 
+        $mail->Password = 'o5584Ac106W';
+        $mail->SMTPSecure = 'ssl';
+        $mail->Port = 465;
+
+        $mail->setFrom('galatea937orin@hotmail.com', 'Franco');
+        $mail->addAddress($email);
+        $mail->addReplyTo('galatea937orin@hotmail.com', 'Información');
+
+        $mail->isHTML(true);
+        $mail->Subject = $subject;
+        $mail->Body = $message;
+        $mail->AltBody = strip_tags($message);
+
+        $mail->addAttachment('./pdfs/' . $nombreArchivo);
+
+        $mail->send();
+        echo 'El mensaje ha sido enviado con éxito';
+    } catch (Exception $e) {
+        echo "El mensaje no pudo ser enviado. Error de PHPMailer: {$mail->ErrorInfo}";
+    }
+}
+?>
