f4868658b3999c93dbbcbb12e0eae7b1893982c9
Nesgren <francozuccorononno@gmail.com>
2024-08-08 13:26:02 +0200

    JSON

---
+++ MODULO_2/PHP/029_Guardar_JSON/anadirCoche.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Resultado de Añadir Coche</title>
+	<link rel="stylesheet" type="text/css" href="../php.css">
+</head>
+<body>	
+	<pre>
+	<?php
+	$fichero = file_get_contents('coches.json');
+	$coches = json_decode($fichero, true);
+	echo '$coches antes de añadir:<br>';
+	print_r($coches);
+
+	$coche = array(
+		'Marca' => $_POST['marca'],
+		'Modelo' => $_POST['modelo'],
+		'Año' => $_POST['ano'],
+		'Color' => $_POST['color']
+	);
+
+	$coches[] = $coche;
+	echo '$coches después de añadir un coche:<br>';
+	print_r($coches);
+
+	$cochesJSON = json_encode($coches, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+	file_put_contents("coches.json", $cochesJSON);
+	?>
+	</pre>
+</body>
+</html>
---
+++ MODULO_2/PHP/029_Guardar_JSON/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Formulario de Coches</title>
+	<link rel="stylesheet" type="text/css" href="../php.css">
+</head>
+<body>	
+	<form action="anadirCoche.php" method="post">
+		<label>Marca:</label> 
+		<input type="text" name="marca" required> 
+		<label>Modelo:</label> 
+		<input type="text" name="modelo" required> 
+		<label>Año:</label> 
+		<input type="number" name="ano" min="1886" required> 
+		<label>Color:</label> 
+		<input type="text" name="color" required> 
+		<input type="submit" value="Añadir coche">
+	</form>
+</body>
+</html>
