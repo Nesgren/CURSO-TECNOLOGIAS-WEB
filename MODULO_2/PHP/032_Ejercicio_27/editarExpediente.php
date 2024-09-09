08d2d53f38768cc65ba1894706cb96763ff96bba
Nesgren <francozuccorononno@gmail.com>
2024-09-09 09:37:53 +0200

    Editar fix

--- MODULO_2/PHP/032_Ejercicio_27/editarExpediente.php
+++ MODULO_2/PHP/032_Ejercicio_27/editarExpediente.php
@@ -32,39 +32,77 @@ $expediente = array_shift($expediente);
 
     <form action="actualizarExpediente.php" method="post" enctype="multipart/form-data">
         <input type="hidden" name="id" value="<?php echo htmlspecialchars($expediente['id']); ?>">
-        <label for="nombre">Nombre:</label>
-        <input type="text" id="nombre" name="nombre" value="<?php echo htmlspecialchars($expediente['Nombre']); ?>" required><br>
 
-        <label for="apellido1">Primer Apellido:</label>
-        <input type="text" id="apellido1" name="apellido1" value="<?php echo htmlspecialchars($expediente['PrimerApellido']); ?>" required><br>
+        <fieldset>
+            <legend>Datos Personales</legend>
+            <label for="nombre">Nombre:</label>
+            <input type="text" id="nombre" name="nombre" value="<?php echo htmlspecialchars($expediente['Nombre']); ?>" required>
 
-        <label for="apellido2">Segundo Apellido:</label>
-        <input type="text" id="apellido2" name="apellido2" value="<?php echo htmlspecialchars($expediente['SegundoApellido']); ?>"><br>
+            <label for="apellido1">Primer Apellido:</label>
+            <input type="text" id="apellido1" name="apellido1" value="<?php echo htmlspecialchars($expediente['PrimerApellido']); ?>" required>
 
-        <label for="email">Email:</label>
-        <input type="email" id="email" name="email" value="<?php echo htmlspecialchars($expediente['Email']); ?>" required><br>
+            <label for="apellido2">Segundo Apellido:</label>
+            <input type="text" id="apellido2" name="apellido2" value="<?php echo htmlspecialchars($expediente['SegundoApellido']); ?>">
 
-        <label for="actitud">Actitud:</label>
-        <input type="text" id="actitud" name="actitud" value="<?php echo htmlspecialchars($expediente['Actitud']); ?>"><br>
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email" value="<?php echo htmlspecialchars($expediente['Email']); ?>" required>
+        </fieldset>
 
-        <label for="idiomas">Idiomas (separados por coma):</label>
-        <input type="text" id="idiomas" name="idiomas" value="<?php echo htmlspecialchars(implode(', ', $expediente['Idiomas'])); ?>"><br>
-
-        <label for="actividades">Actividades (nombre, nota, comentario):</label>
-        <?php foreach ($expediente['Actividades'] as $index => $actividad): ?>
-            <div>
-                <h4>Actividad <?php echo $index + 1; ?></h4>
-                <label>Nombre:</label>
-                <input type="text" name="actividad[<?php echo $index; ?>][nombre]" value="<?php echo htmlspecialchars($actividad['nombre']); ?>"><br>
-
-                <label>Nota:</label>
-                <input type="text" name="actividad[<?php echo $index; ?>][nota]" value="<?php echo htmlspecialchars($actividad['nota']); ?>"><br>
-
-                <label>Comentario:</label>
-                <input type="text" name="actividad[<?php echo $index; ?>][comentario]" value="<?php echo htmlspecialchars($actividad['comentario']); ?>"><br>
+        <fieldset>
+            <legend>Actitud del Alumno en Clase</legend>
+            <div class="actitud">
+                <?php
+                $actitudes = ['Buena', 'Normal', 'Mala'];
+                foreach ($actitudes as $value): ?>
+                    <label>
+                        <input type="radio" name="actitud" value="<?php echo htmlspecialchars($value); ?>" <?php echo $expediente['Actitud'] === $value ? 'checked' : ''; ?>>
+                        <?php echo htmlspecialchars($value); ?>
+                    </label>
+                <?php endforeach; ?>
             </div>
-        <?php endforeach; ?>
-        <input type="file" name="uploadedFile"><br>
+        </fieldset>
+
+        <fieldset>
+            <legend>Idiomas que Habla</legend>
+            <div class="idiomas">
+                <?php
+                $todosIdiomas = ['Catalan', 'Castellano', 'Frances', 'Ingles'];
+                foreach ($todosIdiomas as $idioma): ?>
+                    <label>
+                        <input type="checkbox" name="idiomas[]" value="<?php echo htmlspecialchars($idioma); ?>" <?php echo in_array($idioma, $expediente['Idiomas']) ? 'checked' : ''; ?>>
+                        <?php echo htmlspecialchars($idioma); ?>
+                    </label>
+                <?php endforeach; ?>
+            </div>
+        </fieldset>
+
+        <fieldset>
+            <legend>Actividades</legend>
+            <?php foreach ($expediente['Actividades'] as $index => $actividad): ?>
+                <div class="actividad">
+                    <h4>Actividad <?php echo $index + 1; ?></h4>
+                    <label for="actividad_<?php echo $index; ?>_nombre">Nombre del Ejercicio:</label>
+                    <input type="text" id="actividad_<?php echo $index; ?>_nombre" name="actividad[<?php echo $index; ?>][nombre]" value="<?php echo htmlspecialchars($actividad['nombre']); ?>" required>
+
+                    <label for="actividad_<?php echo $index; ?>_nota">Nota:</label>
+                    <select id="actividad_<?php echo $index; ?>_nota" name="actividad[<?php echo $index; ?>][nota]" required>
+                        <option value="">Selecciona una nota</option>
+                        <?php for ($i = 1; $i <= 10; $i++): ?>
+                            <option value="<?php echo $i; ?>" <?php echo $actividad['nota'] == $i ? 'selected' : ''; ?>><?php echo $i; ?></option>
+                        <?php endfor; ?>
+                    </select>
+
+                    <label for="actividad_<?php echo $index; ?>_comentario">Comentario:</label>
+                    <input type="text" id="actividad_<?php echo $index; ?>_comentario" name="actividad[<?php echo $index; ?>][comentario]" value="<?php echo htmlspecialchars($actividad['comentario']); ?>">
+                </div>
+            <?php endforeach; ?>
+        </fieldset>
+
+        <fieldset>
+            <legend>Subida de Archivos</legend>
+            <label for="uploadedFile">Sube un Archivo:</label>
+            <input type="file" id="uploadedFile" name="uploadedFile">
+        </fieldset>
 
         <input type="submit" value="Actualizar Expediente">
     </form>
