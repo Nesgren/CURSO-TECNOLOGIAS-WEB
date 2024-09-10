33a4cc0b104e05373a678d8365634795ef0f69da
Nesgren <francozuccorononno@gmail.com>
2024-09-10 09:32:18 +0200

    tuCoche

--- MODULO_2/PHP/033_Objetos/Ejemplo1.php
+++ MODULO_2/PHP/033_Objetos/Ejemplo1.php
@@ -15,3 +15,13 @@ $miCoche->potencia = 1.6;
 echo $miCoche->color. "<br>";
 echo $miCoche->marca. "<br>";
 echo $miCoche->modelo. "<br>";
+
+$tuCoche = new Coche();
+$tuCoche->color = "Azul";
+$tuCoche->marca = "Peugeot";
+$tuCoche->modelo = "308";
+$tuCoche->potencia = 2.0;
+
+echo $tuCoche->color. "<br>";    
+echo $tuCoche->marca. "<br>";
+echo $tuCoche->modelo. "<br>";
