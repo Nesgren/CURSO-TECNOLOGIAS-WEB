7a28d8d0f962b0f2869290242f72c94131ecdc6d
Nesgren <francozuccorononno@gmail.com>
2024-09-27 10:55:25 +0200

    fix

--- MODULO_2/PHP/041_Ejercicio_31/views/expedientes/editar.php
+++ MODULO_2/PHP/041_Ejercicio_31/views/expedientes/editar.php
@@ -88,7 +88,7 @@
                 <label for="uploadedFile">Foto del Alumno:</label>
                 <input type="file" id="uploadedFile" name="uploadedFile">
                 <?php if ($expediente->archivo): ?>
-                    <p>Foto <img src="https://franco.104cubes.com/MODULO_2/PHP/041_Ejercicio_31/uploads/<?= htmlspecialchars($expediente->archivo); ?>" class="foto-alumno" alt="Foto Alumno"></p>
+                    <p>Foto <br><img src="https://franco.104cubes.com/MODULO_2/PHP/041_Ejercicio_31/uploads/<?= htmlspecialchars($expediente->archivo); ?>" class="foto-alumno" alt="Foto Alumno"></p>
                 <?php endif; ?>
             </fieldset>
 
