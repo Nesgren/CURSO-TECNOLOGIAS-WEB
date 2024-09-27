84099b64822bba41447e283d9b09b24b3228f95e
Nesgren <francozuccorononno@gmail.com>
2024-09-27 10:32:34 +0200

    Styles added

--- MODULO_2/PHP/041_Ejercicio_31/views/expedientes/editar.php
+++ MODULO_2/PHP/041_Ejercicio_31/views/expedientes/editar.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Editar Expediente</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="../../css/styles.css">
 </head>
 <body>
     <h1>Editar Expediente</h1>
