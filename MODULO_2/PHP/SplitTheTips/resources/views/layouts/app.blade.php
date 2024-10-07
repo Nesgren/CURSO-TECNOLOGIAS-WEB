483944f056769b03c2fad676c5dae8c1dc59bf2c
Nesgren <francozuccorononno@gmail.com>
2024-10-07 12:28:16 +0200

    Fix

--- MODULO_2/PHP/SplitTheTips/resources/views/layouts/app.blade.php
+++ MODULO_2/PHP/SplitTheTips/resources/views/layouts/app.blade.php
@@ -159,8 +159,8 @@
                                     <a class="dropdown-item" href="{{ route('employee.profile') }}">Mi Perfil</a>
                                 @endif
                                 <a class="dropdown-item" href="{{ route('logout') }}"
-                                    onclick="event.preventDefault();
-                                                  document.getElementById('logout-form').submit();">
+                                   onclick="event.preventDefault();
+                                                 document.getElementById('logout-form').submit();">
                                     {{ __('Logout') }}
                                 </a>
 
