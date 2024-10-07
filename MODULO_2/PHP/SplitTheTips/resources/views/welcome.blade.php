206792d7d7a42b26b9442edaf0d501964cc9d532
Nesgren <francozuccorononno@gmail.com>
2024-10-07 12:16:54 +0200

    fix

--- MODULO_2/PHP/SplitTheTips/resources/views/welcome.blade.php
+++ MODULO_2/PHP/SplitTheTips/resources/views/welcome.blade.php
@@ -8,9 +8,19 @@
         <p>La mejor manera de gestionar y distribuir propinas.</p>
     </div>
     @if(Auth::check())
-    <div class="alert alert-info">
-        Usuario actual: {{ Auth::user()->name }}<br>
-        Rol: {{ Auth::user()->role }}<br>
-    </div>
-@endif
+        <div class="alert alert-info">
+            Usuario actual: {{ Auth::user()->name ?? 'Sin nombre' }}<br>
+            Rol: {{ Auth::user()->role ?? 'Sin rol' }}<br>
+            @if(Auth::user()->role === 'employee')
+                <div>
+                    @if(Auth::user()->employee)
+                        <p>ID Empleado: {{ Auth::user()->employee->id ?? 'No asignado' }}</p>
+                        <p>Nombre de la Empresa: {{ Auth::user()->employee->company->name ?? 'No asignada' }}</p>
+                    @else
+                        <span class="text-danger">No se encontró el perfil del empleado.</span>
+                    @endif
+                </div>
+            @endif
+        </div>
+    @endif
 @endsection
